9b73f27e123344607b79d8f19192cc5dc0378931
muthoni12 <muthoningarachu02@gmail.com>
2021-06-23 21:49:07 -0700

    initial commit

---
+++ whileloops.py
@@ -0,0 +1,26 @@
+'''
+c = 0
+while c < 10:
+    print(c)
+    c = c + 1
+'''
+
+c = 0
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+while c < len(numbers):
+    print(numbers[c])
+    c = c + 1
+
+print(len(numbers))
+
+
+
+
+
+
+
+
+
+
+
+
